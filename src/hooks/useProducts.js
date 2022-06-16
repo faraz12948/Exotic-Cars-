6102fe6da39c9e81514a0a85891cf73797303bcb
faraz12948 <faraz15-12948@diu.edu.bd>
2022-06-16 09:04:18 +0600

    useProducts hook used for data fetching in Products , Explore component

--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -1,25 +1,30 @@
 import { useEffect, useState } from "react"
+import useAuth from "./useAuth";
+import useFirebase from "./useFirebase";
 
-const useProducts = () =>{
-    const [products,setProducts] = useState([])
-   
-    
-    useEffect(()=>{
-       
+const useProducts = () => {
+    const [products, setProducts] = useState([])
+    const { setIsLoading } = useFirebase();
+
+
+    useEffect(() => {
+        setIsLoading(true)
         fetch('https://safe-brushlands-03209.herokuapp.com/products')
-        .then(res=>res.json())
-        .then(data=>{setProducts(data)
-           
-        
-        })
+            .then(res => res.json())
+            .then(data => {
+                setProducts(data)
+                setIsLoading(false);
 
-    },[])
+            })
+
+    }, [])
+    // con
 
 
-    return{
+    return {
         products,
         setProducts
     }
-   
+
 }
 export default useProducts;
